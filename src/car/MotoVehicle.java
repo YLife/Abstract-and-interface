be7a60115db3529e3d908048ff98bafd37021c8c
YLife <359059275@qq.com>
2016-12-02 23:38:49 +0800

    Abstract and interface

---
+++ src/car/MotoVehicle.java
@@ -0,0 +1,29 @@
+package car;
+
+public abstract class MotoVehicle {
+	private String no;
+	
+	public abstract double getDailyRent();
+	
+	
+	
+	public MotoVehicle() {
+		
+	}
+	
+	public MotoVehicle(String no){
+		this.no=no;
+	}
+
+	public double calcRent(int day){
+		return day*getDailyRent();
+	}
+
+	public String getNo() {
+		return no;
+	}
+
+	public void setNo(String no) {
+		this.no = no;
+	}
+}
